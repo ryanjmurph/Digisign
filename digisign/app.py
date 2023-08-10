7a72dd8eeeed1f2360d0539abfe80c5f6ac8c7e9
Pius Gumo <pius@piusgumo.me>
2023-08-10 20:28:07 +0200

    changing web server configuration

--- digisign/app.py
+++ digisign/app.py
@@ -2,9 +2,92 @@ from flask import Flask, render_template, request, redirect,Response
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import LargeBinary
 
+from dotenv import load_dotenv
 
+config = load_dotenv() # read the database credentials from .env file
+
+## STEP 1: Create a Flask app
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///files.db'
+
+## STEP 2: Configure the database connection using the username and password from .env file
+app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql+pymysql://{config['MYSQL_USER']}:{config['MYSQL_PASSWORD']}@localhost:3306/{config['MYSQL_DATABASE']}"
+db = SQLAlchemy(app)
+
+## STEP 3: Create models for the database
+## Models: User - Model,Post - Model,Device - Model,Group - Model,GroupModerator - Pivot
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    posts = db.relationship('Post', backref='user', lazy=True)
+
+    def __repr__(self):
+        return f"<User {self.name}>"
+    
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    content = db.Column(db.Text)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return f"<Post {self.title}>"
+    
+class Device(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    groups = db.relationship('Group', secondary='group_device', backref='devices', lazy=True)
+
+    def __repr__(self):
+        return f"<Device {self.name}>"
+    
+class Group(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    moderators = db.relationship('User', secondary='group_moderator', backref='groups', lazy=True)
+
+    def __repr__(self):
+        return f"<Group {self.name}>"
+    
+class GroupModerator(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    group_id = db.Column(db.Integer, db.ForeignKey('group.id'), nullable=False)
+
+    def __repr__(self):
+        return f"<GroupModerator {self.id}>"
+    
+group_device = db.Table('group_device',
+    db.Column('group_id', db.Integer, db.ForeignKey('group.id'), primary_key=True),
+    db.Column('device_id', db.Integer, db.ForeignKey('device.id'), primary_key=True)
+)
+
+group_moderator = db.Table('group_moderator',
+    db.Column('group_id', db.Integer, db.ForeignKey('group.id'), primary_key=True),
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id'), primary_key=True)
+)
+
+## STEP 4: Create routes for the app
+# Create/Post Route
+@app.route('/post/create',methods=['GET','POST'])
+def create_post():
+    if request.method == 'GET':
+        # return the form to create a post
+        return render_template('posts/create.html')
+    elif request.method == 'POST':
+        # create the post
+        title = request.form['title']
+        content = request.form['content']
+        user_id = request.form['user_id']
+        new_post = Post(title=title,content=content,user_id=user_id)
+        db.session.add(new_post)
+        db.session.commit()
+        return redirect('/post')
+    
+
+
+
+
+
 db = SQLAlchemy(app)
 
 class Image(db.Model): # create a class that should be added to DB
