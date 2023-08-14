c62328e0496533fe6e6e09d83dc4415e52f1a652
Pius Gumo <pius@piusgumo.me>
2023-08-14 11:13:52 +0200

    demo updating of devices

--- digisign/app.py
+++ digisign/app.py
@@ -1,142 +1,104 @@
-from flask import Flask, render_template, request, redirect,Response
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import LargeBinary
+import datetime
+from flask import Flask, render_template, request, redirect, Response, jsonify
+import pymysql
 
-from dotenv import load_dotenv
+from dotenv import dotenv_values
 
-config = load_dotenv() # read the database credentials from .env file
+config = dotenv_values(".env")  # read the database credentials from .env file
 
 ## STEP 1: Create a Flask app
 app = Flask(__name__)
 
 ## STEP 2: Configure the database connection using the username and password from .env file
-app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql+pymysql://{config['MYSQL_USER']}:{config['MYSQL_PASSWORD']}@localhost:3306/{config['MYSQL_DATABASE']}"
-db = SQLAlchemy(app)
-
-## STEP 3: Create models for the database
-## Models: User - Model,Post - Model,Device - Model,Group - Model,GroupModerator - Pivot
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
-    posts = db.relationship('Post', backref='user', lazy=True)
-
-    def __repr__(self):
-        return f"<User {self.name}>"
-    
-class Post(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100), nullable=False)
-    content = db.Column(db.Text)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-
-    def __repr__(self):
-        return f"<Post {self.title}>"
-    
-class Device(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
-    groups = db.relationship('Group', secondary='group_device', backref='devices', lazy=True)
-
-    def __repr__(self):
-        return f"<Device {self.name}>"
-    
-class Group(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
-    moderators = db.relationship('User', secondary='group_moderator', backref='groups', lazy=True)
-
-    def __repr__(self):
-        return f"<Group {self.name}>"
-    
-class GroupModerator(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    group_id = db.Column(db.Integer, db.ForeignKey('group.id'), nullable=False)
-
-    def __repr__(self):
-        return f"<GroupModerator {self.id}>"
-    
-group_device = db.Table('group_device',
-    db.Column('group_id', db.Integer, db.ForeignKey('group.id'), primary_key=True),
-    db.Column('device_id', db.Integer, db.ForeignKey('device.id'), primary_key=True)
+connection = pymysql.connect(
+    host="127.0.0.1",
+    user=config["MYSQL_USER"],
+    password=config["MYSQL_PASSWORD"],
+    db=config["MYSQL_DATABASE"],
+    charset="utf8mb4",
+    cursorclass=pymysql.cursors.DictCursor,
 )
 
-group_moderator = db.Table('group_moderator',
-    db.Column('group_id', db.Integer, db.ForeignKey('group.id'), primary_key=True),
-    db.Column('user_id', db.Integer, db.ForeignKey('user.id'), primary_key=True)
-)
+## STEP 3: Run Database Migrations
+# Create User Table
+
+user_table = "CREATE TABLE IF NOT EXISTS user (id INT(11) NOT NULL AUTO_INCREMENT, name VARCHAR(100) NOT NULL, PRIMARY KEY (id)) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci"
+posts_table = "CREATE TABLE IF NOT EXISTS post (id INT(11) NOT NULL AUTO_INCREMENT, title VARCHAR(100) NOT NULL, content TEXT, user_id INT(11) NOT NULL, PRIMARY KEY (id), FOREIGN KEY (user_id) REFERENCES user (id)) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci"
+devices_table = "CREATE TABLE IF NOT EXISTS device (id INT(11) NOT NULL AUTO_INCREMENT, name VARCHAR(100) NOT NULL, PRIMARY KEY (id)) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci"
+groups_table = "CREATE TABLE IF NOT EXISTS `group` (id INT(11) NOT NULL AUTO_INCREMENT, name VARCHAR(100) NOT NULL, PRIMARY KEY (id)) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci"
+group_devices_table = "CREATE TABLE IF NOT EXISTS group_device (id INT(11) NOT NULL AUTO_INCREMENT, device_id INT(11) NOT NULL, group_id INT(11) NOT NULL, PRIMARY KEY (id), FOREIGN KEY (device_id) REFERENCES device (id), FOREIGN KEY (group_id) REFERENCES `group` (id)) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci"
+group_moderator_table = "CREATE TABLE IF NOT EXISTS group_moderator (id INT(11) NOT NULL AUTO_INCREMENT, user_id INT(11) NOT NULL, group_id INT(11) NOT NULL, PRIMARY KEY (id), FOREIGN KEY (user_id) REFERENCES user (id), FOREIGN KEY (group_id) REFERENCES `group` (id)) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci"
+
+with connection.cursor() as cursor:
+    cursor.execute(user_table)
+    cursor.execute(posts_table)
+    cursor.execute(devices_table)
+    cursor.execute(groups_table)
+    cursor.execute(group_devices_table)
+    cursor.execute(group_moderator_table)
+    connection.commit()
+
+
+# Post Model
+class Post:
+    id = None
+    title = None
+    content = None
+    user_id = None
+    start_date = None
+    end_date = None
+
+    def __init__(self, title, content, user_id, start_date, end_date):
+        self.title = title
+        self.content = content
+        self.user_id = user_id
+        # set start_date and end_date to Today's date if not provided
+        self.start_date = (
+            start_date if start_date else datetime.now().strftime("%Y-%m-%d")
+        )
+        self.end_date = end_date if end_date else datetime.now().strftime("%Y-%m-%d")
+
+    def __repr__(self):
+        return "<Post %r>" % self.title
+
+    def insert(self):
+        with connection.cursor() as cursor:
+            sql = "INSERT INTO post (title, content, user_id,start_date,end_date) VALUES (%s, %s, %s, %s, %s)"
+            cursor.execute(
+                sql,
+                (
+                    self.title,
+                    self.content,
+                    self.user_id,
+                    self.start_date,
+                    self.end_date,
+                ),
+            )
+            connection.commit()
+            self.id = cursor.lastrowid
+        return self
+
 
 ## STEP 4: Create routes for the app
 # Create/Post Route
-@app.route('/post/create',methods=['GET','POST'])
+@app.route("/post/create", methods=["GET", "POST"])
 def create_post():
-    if request.method == 'GET':
+    if request.method == "GET":
         # return the form to create a post
-        return render_template('posts/create.html')
-    elif request.method == 'POST':
+        return render_template("posts/create.html")
+    elif request.method == "POST":
         # create the post
-        title = request.form['title']
-        content = request.form['content']
-        user_id = request.form['user_id']
-        new_post = Post(title=title,content=content,user_id=user_id)
-        db.session.add(new_post)
-        db.session.commit()
-        return redirect('/post')
-    
+        title = request.form["title"]
+        content = request.form["content"]
+        user_id = request.form["user_id"]
+        new_post = Post(title=title, content=content, user_id=user_id)
+        new_post.insert()
+        return redirect("/post")
 
-
-
-
-
-db = SQLAlchemy(app)
-
-class Image(db.Model): # create a class that should be added to DB
-    id = db.Column(db.Integer, primary_key=True)
-    filename = db.Column(db.String(100), nullable=False)
-    mimetype = db.Column(db.String(100), nullable=False)
-    content = db.Column(LargeBinary, nullable=False)
-
-    def __repr__(self):
-        return f"<UploadedFile {self.filename}>"
-
-@app.route('/')
-def index(): #initial page to be loaded
-    return render_template('index.html')
-
-@app.route('/upload_file')
-def upload_file():
-    return render_template('page.html')
-
-@app.route('/upload_url')
-def upload_url():
-    return render_template('page.html')
-
-@app.route('/upload', methods=['POST'])
-def upload():
-    if request.method == 'POST':
-        file = request.files['file']
-        if file:
-            filename = file.filename
-            mimetype = file.mimetype
-            content = file.read() # setting values of the uploaded image to these variables
-
-            new_file = Image(filename=filename,mimetype=mimetype, content=content) # making an image object
-            db.session.add(new_file)
-            db.session.commit() # add to db
-
-            return redirect('/')
-    return redirect('/upload_file')
-
-@app.route('/<int:id>') #allows the image to be retrieved by number e.g localhost:5000/1 for the first image in the db
-def getImage(id):
-    img = Image.query.filter_by(id=id).first()
-    if not img:
-        return "There is no such image",404
-    return Response(img.content, mimetype=img.mimetype) # displays image
-    
-    
 
 if __name__ == "__main__":
-    with app.app_context():
-        db.create_all() #creates image
-    app.run(debug=True) 
+    print(
+        "Starting Python Flask Server Digisign. The webapp will run on port "
+        + config["FLASK_PORT"]
+    )
+    app.run(debug=True, port=config["FLASK_PORT"])
