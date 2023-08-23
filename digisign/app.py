4f2f7eeda06fe5e0881a11052093d3dd8e1e240b
Pius Gumo <pius@piusgumo.me>
2023-08-23 17:05:19 +0200

    adding home controller and placeholder home page

--- digisign/app.py
+++ digisign/app.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template, request, redirect, Response, jsonify
 
 from controllers import PostsController
+from controllers import HomeController
 
 from dotenv import dotenv_values
+
 config = dotenv_values(".env")  # read the database credentials from .env file
 
 ## STEP 1: Create a Flask app
@@ -11,10 +13,12 @@ app = Flask(__name__)
 ## STEP 2: Get Database Connection
 
 from database.database import MYSQL
+
 connection = MYSQL().get_connection()
 
 ## Step 3: Register Blueprints
 app.register_blueprint(PostsController.controller, url_prefix="/posts")
+app.register_blueprint(HomeController.controller, url_prefix="/")
 
 ## Step 4: Print the URL map
 print(app.url_map)
