e00da46fe2b7079aa6c773887aa4941fee665afb
Pius Gumo <swagzmania@gmail.com>
2023-09-03 08:50:35 +0200

    fix missing users controller not loaded in app.py

--- digisign/app.py
+++ digisign/app.py
@@ -46,13 +46,14 @@ def unauthorized():
 
 if __name__ == "__main__":
     ## STEP 4: Import Controllers
-    from controllers import AuthenticationController, HomeController, PostsController,GroupsController
+    from controllers import AuthenticationController, HomeController, PostsController,GroupsController,UserController
 
     ## Step 4: Register Blueprints
     app.register_blueprint(PostsController.controller, url_prefix="/posts")
     app.register_blueprint(HomeController.controller, url_prefix="/")
     app.register_blueprint(AuthenticationController.controller, url_prefix="/auth")
     app.register_blueprint(GroupsController.controller, url_prefix="/groups")
+    app.register_blueprint(UserController.controller, url_prefix="/users")
     print(
         "Starting Python Flask Server Digisign. The webapp will run on port "
         + config["FLASK_PORT"]
